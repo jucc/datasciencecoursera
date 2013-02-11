356d05ea09685193da14243ec9061d0c92026a39
Juliana Correa <jucorrea.mail@gmail.com>
2013-02-10 22:56:29 -0200

    added scripts to solve week 1 quiz

--- week1.r
+++ week1.r
@@ -48,3 +48,21 @@ sample(pop, size=4, replace=FALSE,prob=probs)
 treat1 <- sample(1:5, size=3, replace=FALSE)
 treat2 <- sample(6:10, size=3, replace=FALSE)
 c(treat1, treat2)
+
+
+### QUIZ ###
+
+#Question 3
+set.seed(31);
+heightsCM = rnorm(30,mean=188, sd=5);
+weightsK = rnorm(30,mean=84,sd=3); 
+hasDaughter = sample(c(TRUE,FALSE),size=30,replace=T); 
+dataFrame = data.frame(heightsCM,weightsK,hasDaughter); 
+dataFrameSubset <- dataFrame[heightsCM > 188, c(1:3)]
+mean(dataFrameSubset$weightsK)
+
+#Question 4
+set.seed(41)
+cauchyValues <- rcauchy(100)
+set.seed(415)
+sample(cauchyValues, size=10, replace=TRUE)
