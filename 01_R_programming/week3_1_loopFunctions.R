717e5704d7ca64a47e336957ce17b66daf208af2
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2016-04-24 23:31:56 -0300

    finished R programming week 3

--- 01_R_programming/week3_1_loopFunctions.R
+++ 01_R_programming/week3_1_loopFunctions.R
@@ -54,8 +54,30 @@ mapply (rep, 1:4, 4:1)
 
 ## tapply applies functions to subsets 
 
-vec <- c(rnorm(100), runif(100), rnorm(100, 1)) # a vector with three "groups" - normals, uniforms and normals with mean 1
-levels <- gl(3, 100)                            # factor that will categorize the groups in v
-tapply(vec, fact, mean, simplify=TRUE)   # returns the mean for each group
-tapply(vec, fact, range, simplify=FALSE) # returns a list where each element is the range of one of the groups
+vec <- c(rnorm(100), runif(100), rnorm(100, 1)) # a vector with three "groups" - normals, uniforms, normals with mean 1
+levels <- gl(3, 100)                            # factor variable with the categories - 3 levels with 100 elements each
+tapply(vec, levels, mean, simplify=TRUE)   # returns the mean for each group
+tapply(vec, levels, range, simplify=FALSE) # returns a list where each element is the range of one of the groups
 
+
+## split
+
+split(vec, levels)                # returns 3 separate lists
+lapply (split(vec, levels), mean) # same as tapply above, splits into groups an lapply to all of them
+
+months <- split(airquality, airquality$Month) # returns a separate df for each month. 
+# Even though airquality$month is not a factor variable, it can be coerced into it because it's categorical
+
+sapply(months,               # returns the means of all columns for each month
+       function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")], na.rm = TRUE))
+
+# splitting on more than one level
+
+x <- rnorm(10)
+factor1 <- gl(2, 5)  # 2 levels with 5 elements each
+factor2 <- gl(5, 2)  # 5 levels with 2 elements each
+
+allfactors <- interaction(factor1, factor2) # combines levels to create a new factor with 10 different levels
+
+split(x, allfactors)            # returns all 10 lists, one for each combination of factors
+split(x, allfactors, drop=TRUE) # returns only the lists that contain observations
